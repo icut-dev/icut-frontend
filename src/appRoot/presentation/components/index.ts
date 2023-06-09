6be4898df63c6e33a7d47fa2c03931f7ee87e487
Manoel Martins <mano.martins29@gmail.com>
2023-06-09 02:34:38 -0300

    feat: add admin header

--- src/appRoot/presentation/components/index.ts
+++ src/appRoot/presentation/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from './header';
 export { default as InputText } from './form/input-text';
 export { ServiceItem } from './service-item/service-item';
+export { Header as AdminHeader } from './admin/header';
